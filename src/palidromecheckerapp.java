09dd0a98aaa87641e9b72a7ddbc1debf086e5e0c
Aryanimpulse <aryantiwariraipura@gmail.com>
2026-03-05 22:19:34 +0530

    UC13: Performance benchmarking with nanoTime

--- src/palidromecheckerapp.java
+++ src/palidromecheckerapp.java
@@ -469,3 +469,29 @@ class StackStrategy implements PalindromeStrategy {
         return true;
     }
 }
+/**
+ * Use Case 13: Performance Comparison
+ * @author Developer
+ * @version 13.0
+ */
+public class palidromecheckerapp {
+    public static void main(String[] args) {
+        String input = "level";
+
+        long startTime = System.nanoTime();
+
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        long endTime = System.nanoTime();
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + (endTime - startTime) + " ns");
+    }
+}
