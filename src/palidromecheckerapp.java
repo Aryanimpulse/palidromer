6d3ad29df926becf42a48466fb31e65477055181
Aryanimpulse <aryantiwariraipura@gmail.com>
2026-03-05 22:18:29 +0530

    UC12: Strategy pattern implementation

--- src/palidromecheckerapp.java
+++ src/palidromecheckerapp.java
@@ -441,3 +441,31 @@ class PalindromeService {
         return true;
     }
 }
+/**
+ * Use Case 12: Strategy Pattern for Palindrome Algorithms
+ * @author Developer
+ * @version 12.0
+ */
+public class palidromecheckerapp {
+    public static void main(String[] args) {
+        String input = "level";
+        PalindromeStrategy strategy = new StackStrategy();
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + strategy.check(input));
+    }
+}
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String input) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        for (char c : input.toCharArray()) stack.push(c);
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) return false;
+        }
+        return true;
+    }
+}
